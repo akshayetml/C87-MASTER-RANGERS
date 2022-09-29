69e71a4a7c81ad38e80d3941e1c2bed7f6b10909
akshayetml <105727791+akshayetml@users.noreply.github.com>
2022-09-29 20:51:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function new_image(get_image)
 	    block_image_object.scaleToHeight(block_image_height);
         block_image_object.set({
 			top:block_y,
-			top:block_x
+			left:block_x
 		});
 	    canvas.add(block_image_object);
     }) 
